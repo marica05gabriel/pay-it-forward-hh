c5fbfa7eda72264b98a5e9afcde31e87ac53adb4
Gabriel M <marica05gabriel@gmail.com>
2024-03-31 15:22:07 +0300

    import instead of required for Token ignition module

--- ignition/modules/Token.ts
+++ ignition/modules/Token.ts
@@ -1,4 +1,4 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules")
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
 
 const TokenModule = buildModule("TokenModule", (m) => {
     const token = m.contract("Token")
